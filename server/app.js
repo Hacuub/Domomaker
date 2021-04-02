e807aa8d8810cb5328a1de72cae8f2e085a7aed9
Hacuub <jkempsch000@gmail.com>
2021-04-02 14:23:01 -0400

    fixed mongodb

--- server/app.js
+++ server/app.js
@@ -10,7 +10,7 @@ const expressHandlebars = require('express-handlebars');
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
 // const dbURL = process.env.MONGODB_URI || 'mongodb://localhost/DomoMaker';
-const dbURL = 'mongodb+srv://hacuub:Five85585@cluster0.bcwxq.mongodb.net/DomoMaker';
+const dbURL = 'mongodb+srv://hacuub:Five85585@cluster0.twdhr.mongodb.net/Domomaker?retryWrites=true&w=majority';
 const mongooseOptions = {
   useNewUrlParser: true,
   useUnifiedTopology: true,
