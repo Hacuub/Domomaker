9ad850a00bcbcf2e0198d2f61c111b2205688dd4
Hacuub <jkempsch000@gmail.com>
2021-04-10 12:09:24 -0400

    testing on web

--- server/controllers/Account.js
+++ server/controllers/Account.js
@@ -63,19 +63,20 @@ const signup = (request, response) => {
     const newAccount = new Account.AccountModel(accountData);
     const savePromise = newAccount.save();
 
-    newAccount.save().then(() => {
-      // NEW FOR PART B
-     req.session.account = Account.AccountModel.toAPI(newAccount);
-     return res.json({ redirect: '/maker' });
-   }).catch((err) => {
-     console.log(err);
+    newAccount.save((err, doc) => {
+      if (err) {
+        console.log(err);
 
-     if (err.code === 11000) {
-       return res.status(400).json({ error: 'Username already in use.' });
-     }
+        if (err.code === 11000) {
+          return res.status(400).json({ error: 'Username already in use' });
+        }
 
-     return res.status(400).json({ error: 'An error occurred' });
-   });
+        return res.status(400).json({ error: 'An error ocurred' });
+      }
+
+      req.session.account = Account.AccountModel.toAPI(doc);
+      return res.json({ redirect: '/maker' });
+    });
 
     savePromise.then(() => {
       req.session.account = Account.AccountModel.toAPI(newAccount);
